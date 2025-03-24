79252ad91a8367fe8c3aa7d92ae5fe503fc84e74
DragonsLordJacob <jacob.hallburns02@gmail.com>
2025-03-24 18:32:02 -0400

    Update slots.asm
    Currently, you can find the randomized number by adding "selectedChar" to the watch window.
    
    How we can display this character on the command line has not been executed yet

--- slots.asm
+++ slots.asm
@@ -1,11 +1,31 @@
 INCLUDE Irvine32.inc
 .data
    slotCharacters BYTE "0$7", 0
-   
+   randomNumber DWORD ?
+   selectedChar BYTE ?
+
 .code
-
 main PROC
+   call RandomNumberGeneration
 
-   call ExitProcess
+   lea edx, slotCharacters
+   mov ecx, randomNumber
+   add edx, ecx
+   mov al, [edx]
+   mov selectedChar, al
+
+   mov dl, selectedChar
+   call WriteChar
+
+   exit
 main ENDP
+
+RandomNumberGeneration PROC 
+   call Randomize
+
+   mov eax, 3
+   call RandomRange
+   mov randomNumber, eax
+   ret 
+RandomNumberGeneration ENDP
 ENd main
